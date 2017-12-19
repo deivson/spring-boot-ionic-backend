30d57d930dd8f1b7b4d47828cb9a90a4c7dde315
Deivson Gil Souza Lima <deivson.lima10@gmail.com>
2017-12-18 21:49:18 -0300

    Erro de execução

--- src/main/java/com/deivson/cursomc/CursomcApplication.java
+++ src/main/java/com/deivson/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.deivson.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.deivson.cursomc.domain.Cidade;
 import com.deivson.cursomc.domain.Cliente;
 import com.deivson.cursomc.domain.Endereco;
 import com.deivson.cursomc.domain.Estado;
+import com.deivson.cursomc.domain.Pagamento;
+import com.deivson.cursomc.domain.PagamentoComBoleto;
+import com.deivson.cursomc.domain.PagamentoComCartao;
+import com.deivson.cursomc.domain.Pedido;
 import com.deivson.cursomc.domain.Produto;
+import com.deivson.cursomc.domain.enums.EstadoPagamento;
 import com.deivson.cursomc.domain.enums.TipoCliente;
 import com.deivson.cursomc.repositories.CategoriaRepository;
 import com.deivson.cursomc.repositories.CidadeRepository;
 import com.deivson.cursomc.repositories.ClienteRepository;
 import com.deivson.cursomc.repositories.EnderecoRepository;
 import com.deivson.cursomc.repositories.EstadoRepository;
+import com.deivson.cursomc.repositories.PagamentoRepository;
+import com.deivson.cursomc.repositories.PedidoRepository;
 import com.deivson.cursomc.repositories.ProdutoRepository;
 
 
@@ -37,6 +45,10 @@ public class CursomcApplication implements CommandLineRunner {
     private ClienteRepository clienteRepository;
     @Autowired
     private EnderecoRepository endrecoRepository;
+    @Autowired
+    private PedidoRepository pedidoRepository;
+    @Autowired
+    private PagamentoRepository pagamentoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -89,6 +101,21 @@ public class CursomcApplication implements CommandLineRunner {
 		clienteRepository.save(Arrays.asList(cli1));
 		endrecoRepository.save(Arrays.asList(e1,e2));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/12/2017 10:32"),cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/01/2018"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO,ped1,6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/12/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.save(Arrays.asList(ped1,ped2));
+		pagamentoRepository.save(Arrays.asList(pagto1,pagto2));
 		
 		
 		
--- src/main/java/com/deivson/cursomc/domain/Cliente.java
+++ src/main/java/com/deivson/cursomc/domain/Cliente.java
@@ -20,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
-    
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
@@ -28,14 +28,17 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfOuCnpj;
 	private Integer tipo;
-    
+
 	@JsonManagedReference
-	@OneToMany(mappedBy="cliente")
+	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
-    
+
 	@ElementCollection
-	@CollectionTable(name="TELEFONE")
+	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+    
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 
 	public Cliente() {
 	}
@@ -105,6 +108,14 @@ public class Cliente implements Serializable {
 		this.telefones = telefones;
 	}
 
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -129,9 +140,5 @@ public class Cliente implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
 
 }
