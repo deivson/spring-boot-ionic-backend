3fd1f0b69401b7bbbc5f1eb5d9acb4b3a4080550
Deivson Gil Souza Lima <deivson.lima10@gmail.com>
2017-12-21 22:41:55 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/deivson/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.deivson.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.deivson.cursomc.domain.Pedido;
+import com.deivson.cursomc.repositories.PedidoRepository;
+import com.deivson.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: "
+					+id + ", Tipo " + Pedido.class.getName());
+			
+		}
+		return obj;
+	
+	}
+
+}
