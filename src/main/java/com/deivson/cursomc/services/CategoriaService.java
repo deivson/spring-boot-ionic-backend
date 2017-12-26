1f3b874242f84a8c3e7f2f150aa99bc5b37e4755
Deivson Gil Souza Lima <deivson.lima10@gmail.com>
2017-12-26 20:49:55 -0300

    Inserindo um nova Categoria com POST

--- src/main/java/com/deivson/cursomc/services/CategoriaService.java
+++ src/main/java/com/deivson/cursomc/services/CategoriaService.java
@@ -16,12 +16,17 @@ public class CategoriaService {
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
 		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! Id: "
-					+id + ", Tipo " + Categoria.class.getName());
-			
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo " + Categoria.class.getName());
+
 		}
 		return obj;
-	
+
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 
 }
